97cdf1f202be502f95dba647db7f88b4fa31ddf2
ChengQu <ian_qu@outlook.com>
2024-02-15 06:17:02 -0500

    edit transferdata.py - box hand from pic

--- transferdata.py
+++ transferdata.py
@@ -1,24 +1,53 @@
 import os
 import shutil
 import random
+import json
+from PIL import Image
 
-source_dir = "F:/下载/hagrid_dataset_512/hagrid_dataset_512"
-target_dir = "C:/Users/renyi/OneDrive/JHU/Spring 2024/Capstone/553.806_Capstone_HandGesture/dataset"
+source_dir = "E:/MyDatasets/hagrid_dataset_512"
+target_dir = "C:/Users/Ian/git/553.806_Capstone_HandGesture/dataset_boxed"
+annotations_dir = "C:/Users/Ian/git/553.806_Capstone_HandGesture/annotations/train"
 
 os.makedirs(target_dir, exist_ok=True)
 
-for subdir in os.listdir(source_dir):
-    source_subdir = os.path.join(source_dir, subdir)
-    
-    if os.path.isdir(source_subdir):
-        images = os.listdir(source_subdir)
-        
-        selected_images = random.sample(images, min(100, len(images)))
-        
-        target_subdir = os.path.join(target_dir, subdir)
-        os.makedirs(target_subdir, exist_ok=True)
+# Function to read JSON annotations
+def read_annotations(label):
+    annotation_path = os.path.join(annotations_dir, f"{label}.json")
+    with open(annotation_path, 'r') as file:
+        annotations = json.load(file)
+    return annotations
 
-        for image in selected_images:
-            source_image = os.path.join(source_subdir, image)
-            target_image = os.path.join(target_subdir, image)
-            shutil.copy(source_image, target_image)
+# Function to crop image based on the bounding box
+def crop_image(image_path, bbox):
+    with Image.open(image_path) as img:
+        width, height = img.size
+        x0 = int(bbox[0] * width)
+        y0 = int(bbox[1] * height)
+        x1 = x0 + int(bbox[2] * width)
+        y1 = y0 + int(bbox[3] * height)
+        return img.crop((x0, y0, x1, y1))
+
+# Iterate over labels (annotation files)
+for label in os.listdir(annotations_dir):
+    label_name = label.split('.')[0]  # Remove file extension to get the label
+    annotations = read_annotations(label_name)
+
+    selected_images = []
+    keys = list(annotations.keys())
+    selected_keys = random.sample(keys, min(100, len(keys)))
+    for key in selected_keys:
+        image_name_with_ext = f"{key}.jpg"
+        image_path = os.path.join(source_dir, label_name, image_name_with_ext)
+        if os.path.exists(image_path):
+            selected_images.append((image_name_with_ext, annotations[key]['bboxes'][0]))
+    print(label_name, len(selected_images))
+
+    # Copy and crop selected images
+    target_subdir = os.path.join(target_dir, label_name)
+    os.makedirs(target_subdir, exist_ok=True)
+    for image_name, bbox in selected_images:
+        source_image_path = os.path.join(source_dir, label_name, image_name)
+        cropped_image = crop_image(source_image_path, bbox)
+        target_image_path = os.path.join(target_subdir, image_name)
+        cropped_image.save(target_image_path)
+print('Done')
