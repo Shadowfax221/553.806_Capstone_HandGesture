e140d24da99348adba02be8df06f7fd6a963b148
ChengQu <ian_qu@outlook.com>
2024-03-28 18:07:20 -0400

    new code logic on draw landmark

--- live_recognition_mp.py
+++ live_recognition_mp.py
@@ -1,3 +1,4 @@
+import copy
 import time
 
 import cv2
@@ -27,29 +28,41 @@ class Mediapipe_BodyModule():
         self.results = None
 
 
-    def draw_landmarks_on_image(self, rgb_image, detection_result):
-        hand_landmarks_list = detection_result.hand_landmarks
-        #hand_landmarks_list = detection_result
-        annotated_image = np.copy(rgb_image)
-
-        # Loop through the detected poses to visualize.
-        for idx in range(len(hand_landmarks_list)):
-            hand_landmarks = hand_landmarks_list[idx]
-
-            # Draw the pose landmarks.
-            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-            hand_landmarks_proto.landmark.extend([
-                landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-            ])
-            self.mp_drawing.draw_landmarks(annotated_image, hand_landmarks_proto,
-                                                    self.mp_hands.HAND_CONNECTIONS,
-                                                    landmark_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
-                                                        color=(255, 0, 255), thickness=4, circle_radius=2),
-                                                    connection_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
-                                                        color=(20, 180, 90), thickness=2, circle_radius=2)
-            )
+    def draw_landmarks_on_image(self, annotated_image, hand_landmarks):
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+        ])
+        self.mp_drawing.draw_landmarks(annotated_image, hand_landmarks_proto,
+                                    self.mp_hands.HAND_CONNECTIONS,
+                                    landmark_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
+                                        color=(255, 0, 255), thickness=4, circle_radius=2),
+                                    connection_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
+                                        color=(20, 180, 90), thickness=2, circle_radius=2)
+        )
         return annotated_image
     
+    def calc_bounding_rect(image, landmarks):
+        image_width, image_height = image.shape[1], image.shape[0]
+        landmark_array = np.empty((0, 2), int)
+        for _, landmark in enumerate(landmarks.landmark):
+            landmark_x = min(int(landmark.x * image_width), image_width - 1)
+            landmark_y = min(int(landmark.y * image_height), image_height - 1)
+            landmark_point = [np.array((landmark_x, landmark_y))]
+            landmark_array = np.append(landmark_array, landmark_point, axis=0)
+        x, y, w, h = cv.boundingRect(landmark_array)
+        return [x, y, x + w, y + h]
+
+
+    def draw_bounding_rect(self, annotated_image, detection_result):
+        for hand_landmarks in detection_result.hand_landmarks:
+            brect = self.calc_bounding_rect(annotated_image, hand_landmarks)
+        cv2.rectangle(annotated_image, (brect[0], brect[1]), (brect[2], brect[3]),
+                    (0, 0, 0), 1)
+        return annotated_image
+
+
+
     # Create a gesture recognizer instance with the live stream mode:
     def print_result(self, result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
         # print('pose landmarker result: {}'.format(result))
@@ -80,16 +93,17 @@ class Mediapipe_BodyModule():
                     print("Ignoring empty frame")
                     break
                 frame = cv2.flip(frame, 1)
+                annotated_image = copy.deepcopy(frame)
                 
                 timestamp += 1
                 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
                 recognizer.recognize_async(mp_image, timestamp)
 
                 if self.results is not None:
-                    annotated_image = self.draw_landmarks_on_image(mp_image.numpy_view(), self.results)
-                    #cv2.imshow('Show',cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+                    for hand_landmarks in self.results.hand_landmarks:
+                        annotated_image = self.draw_landmarks_on_image(annotated_image, hand_landmarks)
+                        # annotated_image = self.draw_bounding_rect(annotated_image, self.results)
                     cv2.imshow('Show', annotated_image)
-                    # print("showing detected image")
                 else:
                     cv2.imshow('Show', frame)
                 
